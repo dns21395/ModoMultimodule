d29b016705183ec7eff0e8d2ff3e4558dd092693
densis <denis.gabyshev@indriver.com>
2023-02-25 09:55:31 +0600

    added day feature

---
+++ feature/weather/day-api/src/main/java/uk/nightlines/feature/weather/day_api/DayApi.kt
@@ -0,0 +1,8 @@
+package uk.nightlines.feature.weather.day_api
+
+import com.github.terrakok.modo.Screen
+
+interface DayApi {
+
+    fun getDayScreen(): Screen
+}
---
+++ feature/weather/day-impl/src/main/java/uk/nightlines/feature/weather/day_impl/DayScreen.kt
@@ -0,0 +1,12 @@
+package uk.nightlines.feature.weather.day_impl
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DayScreen() {
+    Column {
+        Text("DayScreen")
+    }
+}
