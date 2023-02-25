6ff9515ebac12307250d1419fdcd09078678acf4
densis <denis.gabyshev@indriver.com>
2023-02-25 23:08:00 +0600

    settings one-impl

---
+++ feature/settings/one-impl/src/main/java/uk/nightlines/feature/settings/one_impl/SettingsOneScreen.kt
@@ -0,0 +1,13 @@
+package uk.nightlines.feature.settings.one_impl
+
+import com.github.terrakok.modo.stack.StackNavModel
+import com.github.terrakok.modo.stack.StackScreen
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class SettingsStack(
+    private val stackNavModel: StackNavModel,
+) : StackScreen(stackNavModel) {
+
+    constructor() : this(StackNavModel(emptyList()))
+}
