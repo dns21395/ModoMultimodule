7225a4734f7e0197926f62e0467edc852a8b8b67
densis <denis.gabyshev@indriver.com>
2023-02-25 10:07:53 +0600

    week-impl

---
+++ feature/weather/week-impl/src/main/java/uk/nightlines/feature/weather/week_impl/WeekScreen.kt
@@ -0,0 +1,12 @@
+package uk.nightlines.feature.weather.week_impl
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeekScreen() {
+    Column {
+        Text("WeekScreen")
+    }
+}
