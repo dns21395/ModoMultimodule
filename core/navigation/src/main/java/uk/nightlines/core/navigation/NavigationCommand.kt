18df91a9761c8f740e4f699ebbbcecb58303d8f7
densis <denis.gabyshev@indriver.com>
2023-03-05 11:29:17 +0600

    added eztensions

--- core/navigation/src/main/java/uk/nightlines/core/navigation/NavigationCommand.kt
+++ core/navigation/src/main/java/uk/nightlines/core/navigation/NavigationCommand.kt
@@ -1,3 +1,11 @@
 package uk.nightlines.core.navigation
 
+import com.github.terrakok.modo.Screen
+
 interface NavigationCommand
+
+data class NavigationForward(val screen: Screen, val screens: List<Screen>) : NavigationCommand
+
+data class NavigationReplace(val screen: Screen, val screens: List<Screen>) : NavigationCommand
+
+object NavigationBack : NavigationCommand
---
+++ core/navigation/src/main/java/uk/nightlines/core/navigation/NavigationExtensions.kt
@@ -0,0 +1,15 @@
+package uk.nightlines.core.navigation
+
+import com.github.terrakok.modo.NavigationContainer
+import com.github.terrakok.modo.stack.StackState
+import com.github.terrakok.modo.stack.back
+import com.github.terrakok.modo.stack.forward
+import com.github.terrakok.modo.stack.replace
+
+fun NavigationContainer<StackState>.navigate(command: NavigationCommand) {
+    when (command) {
+        is NavigationForward -> forward(command.screen, *command.screens.toTypedArray())
+        is NavigationReplace -> replace(command.screen, *command.screens.toTypedArray())
+        is NavigationBack -> back()
+    }
+}
