0f3c49736ced6bcc7b5b26a964e06d9245421484
densis <denis.gabyshev@indriver.com>
2023-03-17 11:56:43 +0600

    added new commands

--- core/navigation/src/main/java/uk/nightlines/core/navigation/NavigationCommand.kt
+++ core/navigation/src/main/java/uk/nightlines/core/navigation/NavigationCommand.kt
@@ -7,3 +7,12 @@ interface NavigationCommand
 data class NavigationForward(val screen: Screen, val screens: List<Screen> = emptyList()) : NavigationCommand
 
 data class NavigationReplace(val screen: Screen, val screens: List<Screen> = emptyList()) : NavigationCommand
+
+data class NavigationNewStack(val screens: List<Screen>) : NavigationCommand
+
+data class NavigationRemoveScreen(val positions: List<Int>) : NavigationCommand
+
+object NavigationBackToRoot : NavigationCommand
+
+object NavigationExit : NavigationCommand
+
--- core/navigation/src/main/java/uk/nightlines/core/navigation/NavigationExtensions.kt
+++ core/navigation/src/main/java/uk/nightlines/core/navigation/NavigationExtensions.kt
@@ -1,13 +1,15 @@
 package uk.nightlines.core.navigation
 
 import com.github.terrakok.modo.NavigationContainer
-import com.github.terrakok.modo.stack.StackState
-import com.github.terrakok.modo.stack.forward
-import com.github.terrakok.modo.stack.replace
+import com.github.terrakok.modo.stack.*
 
 fun NavigationContainer<StackState>.navigate(command: NavigationCommand) {
     when (command) {
+        is NavigationNewStack -> setStack(StackState(stack = command.screens))
         is NavigationForward -> forward(command.screen, *command.screens.toTypedArray())
         is NavigationReplace -> replace(command.screen, *command.screens.toTypedArray())
+        is NavigationRemoveScreen -> removeScreen(*command.positions.toIntArray())
+        is NavigationBackToRoot -> backToRoot()
+        is NavigationExit -> exit()
     }
 }
