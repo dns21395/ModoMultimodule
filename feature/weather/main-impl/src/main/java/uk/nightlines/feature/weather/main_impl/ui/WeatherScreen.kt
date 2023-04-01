b550d0cace4ec69c6f8932799cef55a98ca322b1
densis <dns21395@gmail.com>
2023-04-01 19:55:45 +0600

    weather container in external function

--- feature/weather/main-impl/src/main/java/uk/nightlines/feature/weather/main_impl/ui/WeatherScreen.kt
+++ feature/weather/main-impl/src/main/java/uk/nightlines/feature/weather/main_impl/ui/WeatherScreen.kt
@@ -88,6 +88,10 @@ internal class WeatherStack(
         ) {
             WeatherScreenContent(
                 state = state.value,
+                counter = counter.toString(),
+                screenKey = screenKey.value,
+                screenHashCode = hashCode().toString(),
+                navigationStack = navigationState.stack,
                 onShowOptionsButtonClicked = { viewModel.onShowOptionsButtonClicked() },
                 onForwardButtonClicked = { viewModel.onOpenNewWeatherScreenButtonClicked() },
                 onReplaceButtonClicked = { viewModel.onReplaceButtonClicked() },
@@ -98,194 +102,9 @@ internal class WeatherStack(
                 onNewStackButtonClicked = { viewModel.openNewStackButtonClicked() },
                 onMultiForwardButtonClicked = { viewModel.onMultiForwardButtonClicked() },
                 onNewRootButtonClicked = { viewModel.onNewRootButtonClicked() },
-                onContainerButtonClicked = { viewModel.onContainerButtonClicked() })
+                onContainerButtonClicked = { viewModel.onContainerButtonClicked() },
+                topScreenContent =  { TopScreenContent() }
+            )
         }
-    }
-
-    @Composable
-    fun WeatherScreenContent(
-        state: WeatherViewState,
-        onShowOptionsButtonClicked: suspend () -> Unit,
-        onForwardButtonClicked: suspend () -> Unit,
-        onReplaceButtonClicked: suspend () -> Unit,
-        onRemoveByPositionsEditTextChanged: suspend (String) -> Unit,
-        onRemoveByPositionsButtonClicked: suspend () -> Unit,
-        onBackToSecondScreenButtonClicked: suspend (Screen) -> Unit,
-        onBackToRootClicked: suspend () -> Unit,
-        onNewStackButtonClicked: suspend () -> Unit,
-        onMultiForwardButtonClicked: suspend () -> Unit,
-        onNewRootButtonClicked: suspend () -> Unit,
-        onContainerButtonClicked: suspend () -> Unit,
-    ) {
-        val coroutineScope = rememberCoroutineScope()
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(state.backgroundColor)
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .background(Color.Gray)
-                    .padding(2.dp)
-                    .background(Color.White)
-            ) {
-                Text(text = state.emoji, style = MaterialTheme.typography.h4)
-                Text(
-                    text = "WEATHER #$counter (${screenKey.value})\n " +
-                            "CONTAINER HASCODE : ${this@WeatherStack.hashCode()}\n" +
-                            "STACK : ${navigationState.stack.map { it.screenKey.value }}"
-                )
-
-                Button(onClick = { coroutineScope.launch { onShowOptionsButtonClicked() } }) {
-                    Text(if (state.isOptionsVisible) "HIDE OPTIONS" else "SHOW OPTIONS")
-                }
-
-                if (state.isOptionsVisible) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        Button(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 16.dp),
-                            onClick = {
-                                coroutineScope.launch { onForwardButtonClicked() }
-                            }) {
-                            Text(text = "FORWARD")
-                        }
-                        Button(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 16.dp),
-                            onClick = {
-                                coroutineScope.launch { onReplaceButtonClicked() }
-                            }) {
-                            Text(text = "REPLACE")
-                        }
-                    }
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        BasicTextField(
-                            value = state.positionEditText,
-                            onValueChange = { text ->
-                                coroutineScope.launch { onRemoveByPositionsEditTextChanged(text) }
-                            },
-                            decorationBox = { innerTextField ->
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .background(
-                                            color = Color(0xFF5E35B1),
-                                            shape = RoundedCornerShape(size = 16.dp)
-                                        )
-                                        .border(
-                                            width = 2.dp,
-                                            color = Color(0xFFFBC02D),
-                                            shape = RoundedCornerShape(size = 16.dp)
-                                        )
-                                        .padding(all = 16.dp),
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Default.Search,
-                                        contentDescription = "Favorite icon",
-                                        tint = Color.DarkGray
-                                    )
-                                    Spacer(modifier = Modifier.width(width = 8.dp))
-                                    innerTextField()
-                                }
-                            },
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(16.dp)
-                        )
-                        Button(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(16.dp),
-                            onClick = {
-                                coroutineScope.launch { onRemoveByPositionsButtonClicked() }
-                            }) {
-                            Text("REMOVE BY POSITIONS")
-                        }
-                    }
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        Button(
-                            onClick = {
-                                coroutineScope.launch {
-                                    onBackToSecondScreenButtonClicked(
-                                        navigationState.stack[1]
-                                    )
-                                }
-                            },
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 16.dp)
-                        ) {
-                            Text("BACK TO 2ND SCREEN")
-                        }
-                        Button(
-                            onClick = { coroutineScope.launch { onBackToRootClicked() } },
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 16.dp)
-                        ) {
-                            Text("BACK TO ROOT")
-                        }
-                    }
-                    Row {
-                        Button(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 16.dp),
-                            onClick = {
-                                coroutineScope.launch { onNewStackButtonClicked() }
-                            }) {
-                            Text(text = "SET STACK")
-                        }
-                        Button(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 16.dp),
-                            onClick = {
-                                coroutineScope.launch { onMultiForwardButtonClicked() }
-                            }) {
-                            Text(text = "MULTI FORWARD")
-                        }
-                    }
-                    Row {
-                        Button(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 16.dp),
-                            onClick = {
-                                coroutineScope.launch { onNewRootButtonClicked() }
-                            }) {
-                            Text(text = "NEW ROOT")
-                        }
-                        Button(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 16.dp),
-                            onClick = {
-                                coroutineScope.launch { onContainerButtonClicked() }
-                            }) {
-                            Text(text = "CONTAINER")
-                        }
-                    }
-                }
-
-                TopScreenContent()
-            }
-        }
-
     }
 }
