2284d0f30ccac2cd5d17f545b4027c27538a553f
densis <dns21395@gmail.com>
2023-07-04 21:10:23 +0600

    added core:ui module

---
+++ core/ui/src/main/java/uk/nightlines/core/ui/Ui.kt
@@ -0,0 +1,4 @@
+package uk.nightlines.core.ui
+
+interface Ui {
+}
--- feature/weather/week-impl/src/main/java/uk/nightlines/feature/weather/week_impl/ui/WeekViewModel.kt
+++ feature/weather/week-impl/src/main/java/uk/nightlines/feature/weather/week_impl/ui/WeekViewModel.kt
@@ -1,17 +1,15 @@
 package uk.nightlines.feature.weather.week_impl.ui
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import uk.nightlines.core.common.RootScreensCounterInteractor
 import uk.nightlines.core.common.RootScreensInteractor
 import uk.nightlines.core.common.state.SimpleState
-import uk.nightlines.core.navigation.RootNavigationQualifier
-import uk.nightlines.core.navigation.command.NavigationTypeCommand
 import uk.nightlines.core.navigation.NavigationForward
 import uk.nightlines.core.navigation.NavigationReplace
-import uk.nightlines.feature.weather.common.WeatherDependencies
+import uk.nightlines.core.navigation.RootNavigationQualifier
+import uk.nightlines.core.navigation.command.NavigationTypeCommand
 import uk.nightlines.feature.weather.common.WeatherNavigationQualifier
 import uk.nightlines.feature.weather.day_api.DayScreenApi
 import javax.inject.Inject
@@ -19,7 +17,6 @@ import javax.inject.Inject
 internal class WeekViewModel @Inject constructor(
     @RootNavigationQualifier private val rootNavigation: NavigationTypeCommand,
     @WeatherNavigationQualifier private val locatlNavigation: NavigationTypeCommand,
-    private val weatherDependencies: WeatherDependencies,
     private val rootScreens: RootScreensInteractor,
     private val dayApi: DayScreenApi,
     private val weatherScreenCounterInteractor: RootScreensCounterInteractor
@@ -29,7 +26,6 @@ internal class WeekViewModel @Inject constructor(
     val state: StateFlow<SimpleState> = mutableState
 
     suspend fun onReplaceButtonClicked() {
-        Log.d("GTA5", "[WEEK] ViewModel. DEPS : ${weatherDependencies.hashCode()}")
         locatlNavigation.navigate(NavigationReplace(dayApi.getDayScreen()))
     }
 
